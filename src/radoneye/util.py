4b96338045bac0c48c44ea49cde29dd5d51312ab
Artem Butusov <art.sormy@gmail.com>
2024-05-23 23:49:49 -0400

    Better debug dumps

--- src/radoneye/util.py
+++ src/radoneye/util.py
@@ -1,6 +1,6 @@
 import math
+import string
 from struct import unpack
-from textwrap import wrap
 
 
 def read_str_sz(buffer: bytearray, offset: int) -> str:
@@ -46,11 +46,39 @@ def format_uptime(uptime_minutes: int) -> str:
 def format_counts(counts_current: int, counts_previous: int) -> str:
     return f"{counts_current}/{counts_previous}"
 
+
+def is_printable(ch: str):
+    return (
+        ch in string.digits or ch in string.ascii_letters or ch in string.punctuation or ch == " "
+    )
+
+
+def print_byte(b: int) -> str:
+    ch = chr(b)
+    return ch if is_printable(ch) else "."
+
+
+def split_data(data: bytearray, size: int):
+    for i in range(0, len(data), size):
+        yield data[i : i + size]
+
+
+def dump_data_str(data: bytearray) -> str:
+    return "".join([print_byte(b) for b in data])
+
+
 def dump_data(data: bytearray, input: bool, debug: bool) -> bytearray:
     if debug:
-        lines = [f"{line.ljust(48)}# {str(bytearray.fromhex(line))}" for line in wrap(data.hex(" "), width=48)]
-        print(f"{"<-" if input else "->"} {"\n   ".join(lines)}")
+        dir = "<-" if input else "->"
+        text = "\n   ".join(
+            [
+                f"{chunk.hex(" ").ljust(48)}# {dump_data_str(chunk)}"
+                for chunk in split_data(data, 16)
+            ]
+        )
+        print(f"{dir} {text}")
     return data
+
 
 def dump_in(data: bytearray, debug: bool) -> bytearray:
     return dump_data(data, True, debug)
